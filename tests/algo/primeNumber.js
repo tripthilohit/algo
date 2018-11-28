09f1c321524838063d6c97bd91774572bd0b4574
tripthilohit <tripthi.lohit@gmail.com>
2018-11-28 17:22:21 -0500

    changes to prime

--- tests/algo/primeNumber.js
+++ tests/algo/primeNumber.js
@@ -1,31 +1,32 @@
 
-//driver.globals.userNames.memberURL
 
-   //var num =prompt("Enter User Value")
 
-   function isPrime(num) {
-    for ( var i = 2; i < num; i++ ) {
-    	
-        if ( num % i === 0 ) {
+function isPrime(num)
+{
+    for(var i=2; i<num; i++)
+    {
+        if(num %i === 0)
+        {
             return false;
         }
     }
-      return true;
-    
-    
-
-    
+    return true;
 }
 
-function display(n) {
+function display(n)
+{
     var arr = [2];
-    for ( var i = 3; i < n; i+=2 ) {
-        if ( isPrime(i) ) {
-            arr.push(i);
+    for(var i=3;i<n;i+=2)
+    {
+        if(isPrime(i))
+         {
+        arr.push(i);
         }
     }
-    console.log(arr); // use arr result on your own
+        console.log(arr);
 }
 
+
 display(100);
-     
+
+
